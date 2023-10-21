f425203a4fdc4aa87b9d0122eb12e05a48f2560e
jana <jana.ahmedhussein244@gmail.com>
2023-10-21 08:39:38 -0700

    pp

--- 3tools.c
+++ 3tools.c
@@ -11,3 +11,35 @@ while (operation_code_tokens[l])
 l++;
 return (l);
 }
+/**
+ * total_fun - make them all together
+ * @functions: pointer
+ * Return: pointer
+ */
+void (*total_fun(char *functions))(stack_t**, unsigned int)
+{
+instruction_t func[] = {
+{"push", _push},
+{"pall", _pall},
+};
+int x;
+for (x = 0; func[x].functions; x++)
+{
+if (strcmp(functions, func[x].functions) == 0)
+return (functions[x].f);
+}
+return (NULL);
+}
+/**
+ * sserror - for errors
+ * @nline: for line number
+ * @fun: pointer
+ * Return: EXIT_FAILURE
+ */
+int sserror(unsigned int nline, char *fun)
+{
+fprintf(stderr, "L%u: can't %s, stack too short\n", nline, fun);
+return (EXIT_FAILURE);
+}
+int sserror(unsigned int nline, char *fun);
+void (*total_fun(char *functions))(stack_t**, unsigned int);
