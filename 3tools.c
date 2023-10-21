ed81a3d672291911c0ade13e805847b18fa09396
MohgaMA <mohga.m.ashour@gmail.com>
2023-10-21 11:47:16 -0700

    fix error

--- 3tools.c
+++ 3tools.c
@@ -22,7 +22,6 @@ void (*total_fun(char *opcode))(stack_t**, unsigned int)
 instruction_t func[] = {
 {"push", _push},
 {"pall", _pall},
-{"pint", _pint}
 };
 int x;
 for (x = 0; func[x].opcode; x++)
