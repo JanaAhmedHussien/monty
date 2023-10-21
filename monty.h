7b51e9aba484ee39fe410363b5deda7243ac2042
MohgaMA <mohga.m.ashour@gmail.com>
2023-10-21 11:45:18 -0700

    fix errors

--- monty.h
+++ monty.h
@@ -1,6 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MONTY_H
+#define MONTY_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
